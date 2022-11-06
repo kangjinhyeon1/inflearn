25dc6ecb12bfc5bfc67577f306741fb9b3a8528a
강진현 <kangjin627@gmail.com>
2022-11-06 22:41:53 +0900

    [ADD] 프롭스랑 함수 어케쓰는지 등 공부 내용

--- grab-react/src/App.js
+++ grab-react/src/App.js
@@ -1,23 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import ChildComponent from './child.js';
 
 function App() {
+  const text = "기모링!";
+  const sayHello = function(){
+    return <h3>인프런 기모링</h3>
+  }
+  const sayHello2 = function(){
+    alert('안녕');
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>안녕하세요</h1>
+      <h2>{text}</h2>
+      {sayHello()}
+      <div onClick={sayHello2}>클릭해줘잉</div>
+      <ChildComponent name="그랩" age={27}/>
+      <ChildComponent name="철수" age={17}/>
+      <ChildComponent name="길동" age={37}/>
     </div>
   );
 }
