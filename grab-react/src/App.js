99ea84a3e60c52dfac1d30e9f7a915954cdcbb80
강진현 <kangjin627@gmail.com>
2022-11-08 19:58:20 +0900

    [ADD] 그랩 마켓 이미지 추가 및 초기 세팅

--- grab-react/src/App.js
+++ grab-react/src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
-import ChildComponent from './child.js';
+import MainPageComponent from './main';
 
 function App() {
-  const text = "기모링!";
-  const sayHello = function(){
-    return <h3>인프런 기모링</h3>
-  }
-  const sayHello2 = function(){
-    alert('안녕');
-  }
+  
   return (
-    <div>
-      <h1>안녕하세요</h1>
-      <h2>{text}</h2>
-      {sayHello()}
-      <div onClick={sayHello2}>클릭해줘잉</div>
-      <ChildComponent name="그랩" age={27}/>
-      <ChildComponent name="철수" age={17}/>
-      <ChildComponent name="" age={37}/>
-    </div>
+    <MainPageComponent/>
   );
 }
 
